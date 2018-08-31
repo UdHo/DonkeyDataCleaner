cc4b89461c809696ebe232c04aafb8aa73099bb7
Udo Hoffmann <hoffmann.odu@gmail.com>
2018-08-31 02:57:16 +0200

    autoformat

--- clean.py
+++ clean.py
@@ -6,36 +6,38 @@ from PIL import ImageTk, Image
 
 
 class DataCleaner:
-    def __init__(self, path = "."):
+    def __init__(self, path="."):
         self.path = path
         self.window = Tk()
         self.panel = Label(self.window)
         self.angle = Label(self.window)
-        self.imagelist = list(filter( lambda x: x.find(".jpg")!=-1, os.listdir(path)))
+        self.imagelist = list(
+            filter(lambda x: x.find(".jpg") != -1, os.listdir(path)))
         self.imageiter = iter(self.imagelist)
         self.nextImage()
         self.panel.pack()
         self.angle.pack()
         self.window.bind("<Key>", self.key)
         self.window.mainloop()
-        
+
     def nextImage(self):
         self.loadImage(next(self.imageiter))
         self.updateAngle()
-        
+
     def loadImage(self, imagePath):
         self.imagename = imagePath
-        self.image = ImageTk.PhotoImage(Image.open(imagePath))
-        self.panel.config (image = self.image)
+        self.image = ImageTk.PhotoImage(
+            Image.open(self.path + "/" + imagePath))
+        self.panel.config(image=self.image)
 
     def updateAngle(self):
-        jsonfile = "record_" + self.imagename[:self.imagename.find("_")] + ".json"
-        with open(jsonfile) as json_data:
+        jsonfile = "record_" + \
+            self.imagename[:self.imagename.find("_")] + ".json"
+        with open(self.path + "/" + jsonfile) as json_data:
             d = json.load(json_data)
-            self.angle.config(text = d["user/angle"])
+            self.angle.config(text=d["user/angle"])
             print(d)
-    
-    
+
     def key(self, event):
         print("pressed", repr(event.char))
         c = event.char
@@ -53,7 +55,7 @@ class DataCleaner:
                     direction = -1.2 + direction * 0.2
                 else:
                     direction = (direction-5) * 0.2
-                print("New direction",direction)
+                print("New direction", direction)
                 self.changeDirection(direction)
             except ValueError:
                 print("Delete")
@@ -62,30 +64,26 @@ class DataCleaner:
 
     def keep(self):
         f = open("keep.txt", "a+")
-        f.write(self.imagename+"\n")
+        f.write(self.imagename + "\n")
         f.close()
-        
+
     def delete(self):
         f = open("delete.txt", "a+")
-        f.write(self.imagename+"\n")
+        f.write(self.imagename + "\n")
         f.close()
-        
+
     def changeDirection(self, direction):
         f = open("change.txt", "a+")
         f.write(self.imagename + " " + str(direction) + "\n")
         f.close()
-                    
-                    
-                
-            
-            
+
 
 def main(argv):
-    if len(argv)>0:
+    if len(argv) > 0:
         gui = DataCleaner(argv[0])
     else:
         gui = DataCleaner()
-    
+
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
-        
+    main(sys.argv[1:])
